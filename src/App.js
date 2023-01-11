5c5693d814c1c7f242f08864e08b7c7f95eb5fcf
Molly13able <100470278+Molly13able@users.noreply.github.com>
2023-01-11 13:48:21 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./index.css";
+import "./App.css";
+
+import Home from "./components/Home";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchPage from "./components/SearchPage";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="./" element={<Home />}></Route>
+          <Route path="./search" element={<SearchPage />}></Route>
+        </Routes>
+      </Router>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
